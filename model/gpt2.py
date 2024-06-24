aa7bc03373e846ae8b8c33294e8f634a75e0cb14
Luke Martin <lukechile@gmail.com>
2024-06-24 18:55:24 -0400

    fix and add compile

--- model/gpt2.py
+++ model/gpt2.py
@@ -46,7 +46,7 @@ class GPT2Model(nn.Module):
         if isinstance(module, nn.Linear):
             std = 0.02
             if hasattr(module, "NANOGPT_SCALE_INIT"):
-                std *= (2 * self.config.n_layers) ** -0.5
+                std *= (2 * self.config.n_layer) ** -0.5
 
             torch.nn.init.normal_(module.weight, mean=0.0, std=std)
             if module.bias is not None:
--- trainer.py
+++ trainer.py
@@ -21,6 +21,7 @@ class GPT2Lighting(L.LightningModule):
         super().__init__()
 
         self.model = GPT2Model(config.model)
+        self.model = torch,compile(self.model)
         self.config = config
 
     def training_step(self, batch: Tuple[Tensor, Tensor], batch_idx: int):
@@ -35,7 +36,9 @@ class GPT2Lighting(L.LightningModule):
 
         t1 = time.time()
         tokens_per_sec = len(batch) * x.shape[1] / (t1 - t0)
-        self.log("tok/sec", tokens_per_sec)
+
+        self.log("tokens per second", tokens_per_sec)
+        self.log("dt", (t1 - t0) * 1000)
 
         return loss
 
@@ -52,5 +55,9 @@ if __name__ == "__main__":
     dataloader = DataLoader(dataset, batch_size=64, num_workers=16)
     logger = TensorBoardLogger("test_logs", name="testing_model")
 
-    trainer = L.Trainer(max_epochs=100, logger=logger, log_every_n_steps=1)
+    trainer = L.Trainer(
+        precision='32',
+        max_epochs=100, 
+        logger=logger, 
+        log_every_n_steps=1)
     trainer.fit(model=model, train_dataloaders=dataloader)
